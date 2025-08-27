ec0e842877fb1dc83b47813602584cf287c4106c
vighnesh-tirakapadi02 <vighnesh.vijay@pattemdigital.com>
2025-08-27 19:49:32 +0530

    learned args, argTypes & parameters

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,34 +1,92 @@
+import { ChevronLeft, ChevronRight, Loader } from "lucide-react";
 import React, { type ReactNode } from "react";
 
 interface ButtonProps {
   variant: "primary" | "secondary" | "danger" | "success" | "ghost";
   children: ReactNode;
+  disabled: boolean;
+  onClick: () => void;
+  iconPosition?: "left" | "right";
+  size?: "sm" | "md" | "lg";
+  loader?: boolean;
+  icon?: React.ReactNode
 }
 
-const Button = ({ variant, children }: ButtonProps): React.JSX.Element => {
-    let classNames = "";
+const Button = ({
+  variant,
+  children,
+  disabled,
+  onClick,
+  iconPosition = "left",
+  size = "md",
+  loader = false,
+  icon
+}: ButtonProps): React.JSX.Element => {
+  let classNames = "";
 
-    switch (variant) {
-      case "primary":
-        classNames = "bg-blue-500 px-4 py-1 rounded text-white";
-        break;
-      case "secondary":
-        classNames = "bg-violet-500 px-4 py-1 rounded text-white";
-        break;
-      case "danger":
-        classNames = "bg-rose-500 px-4 py-1 rounded text-white";
-        break;
-      case "success":
-        classNames = "bg-green-500 px-4 py-1 rounded text-white";
-        break;
-      case "ghost":
-        classNames = "bg-gray-50 px-4 py-1 rounded text-blue-500";
-        break;
-      default:
-        break;
-    }
+  switch (variant) {
+    case "primary":
+      classNames = "bg-blue-500 rounded text-white";
+      break;
+    case "secondary":
+      classNames = "bg-violet-500 rounded text-white";
+      break;
+    case "danger":
+      classNames = "bg-rose-500 rounded text-white";
+      break;
+    case "success":
+      classNames = "bg-green-500 rounded text-white";
+      break;
+    case "ghost":
+      classNames = "bg-gray-50 rounded text-blue-500";
+      break;
+    default:
+      break;
+  }
 
-  return <button className={classNames}>{children}</button>;
+  let sizeClasses = "";
+  switch (size) {
+    case "sm":
+      sizeClasses = "px-4 py-1 text-sm";
+      break;
+    case "md":
+      sizeClasses = "px-6 py-2 text-base";
+      break;
+    case "lg":
+      sizeClasses = "px-6 py-3 text-lg";
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <button
+      className={`${classNames} ${sizeClasses} ${
+        disabled ? "bg-stone-400 cursor-not-allowed" : ""
+      } flex items-center gap-2`}
+      onClick={onClick}
+    >
+      {iconPosition === "left" && (
+        <span>
+          <ChevronLeft />
+        </span>
+      )}
+      {loader && (
+        <span>
+          <Loader className="w-6 h-6 text-pink-500 animate-spin" />
+        </span>
+      )}
+      {icon && iconPosition === "left" && icon}
+      {children}
+      {icon && iconPosition === "right" && icon}
+      {iconPosition === "right" && (
+        <span>
+          <ChevronRight />
+        </span>
+      )}
+    </button>
+  );
 };
 
 export default Button;
